a54a93ce1c4183414b737aaa8f6b9df8a41a31e4
brunod3vt3c <office.bruno.c.r@gmail.com>
2022-08-15 19:44:33 +0100

    remove image word to the alt img

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import image from "../transferir.png"
 function Home() {
   return ( 
     <main>
-      <img src={image} alt="Ai Image" className="absolute object-cover w-full h-full" />
+      <img src={image} alt="Ai" className="absolute object-cover w-full h-full" />
       <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
         <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">Olá. Eu sou Bruno.</h1>
       </section>
